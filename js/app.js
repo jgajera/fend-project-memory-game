b657fd2cb1da0fd8c0fbf2994e1433cfd11072c1
Janki <jnkdesignhouse@gmail.com>
2018-06-18 19:17:09 -0600

    card flipping added

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@
 
 let cards = [...document.querySelectorAll('.card')];
 const cardNum = cards.length;
-console.log(cards);
-console.log(cardNum);
 
 /*
  * Display the cards on the page
@@ -15,10 +13,6 @@ console.log(cardNum);
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-
-
-
-shuffle(cards);
 
 function shuffle(array) {
     var currentIndex = array.length,
@@ -34,7 +28,9 @@ function shuffle(array) {
     return array;
 }
 
-console.log(cards);
+
+shuffle(cards);
+
 
 // push shuffled array into empty array
 let cardsShuff = [];
@@ -43,16 +39,14 @@ for (let i = 0; i < cardNum; i++) {
     cardsShuff.push(cardContent);
 }
 
-console.log(cardsShuff);
-
 // feed shuffled array into the HTML
 // // // first clear current grid
 const cardDeck = document.querySelector('.deck');
 cardDeck.innerHTML = '';
-console.log(cardDeck);
+
 // // // insert new shuffled array
-for (let i = 0; i < cardNum; i++) {
-    cardDeck.insertAdjacentHTML('afterbegin', cardsShuff[i].outerHTML);
+for (let j = 0; j < cardNum; j++) {
+    cardDeck.insertAdjacentHTML('afterbegin', cardsShuff[j].outerHTML);
 }
 
 
@@ -61,18 +55,31 @@ for (let i = 0; i < cardNum; i++) {
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  */
 
- for (let i = 0; i < cardNum; i++) {
-    cardsShuff[i].addEventListener('click',cardReveal(cardsShuff[i])
-    	);
+let cardSelector = [...document.querySelectorAll('.card')];
+
+let cardShow = [...document.querySelectorAll('.open')];
+
+for (let k = 0; k < cardNum; k++) {
+    cardSelector[k].addEventListener('click', cardReveal)
+};
+
+function cardReveal() {
+    this.classList.add('open', 'show');
+    /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+     */
+    cardShow.push(this);
+    console.log(cardShow);
 }
 
-function cardReveal(eachCard) {
-	eachCard.classList.add('open');
-}
 
- /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+//if (cardShow.length > 2) {
+//    this.classList.remove('open', 'show');
+// }
+
+/*  - if the list already has another card, check to see if the two cards match
+ */
+console.log(cardShow.length);
+/*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
