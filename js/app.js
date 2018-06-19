8e9929f8a7c3550631fd8d663cd7de18e32a6c86
Janki <jnkdesignhouse@gmail.com>
2018-06-18 18:25:43 -0600

    cards shuffling visually

--- js/app.js
+++ js/app.js
@@ -16,11 +16,9 @@ console.log(cardNum);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 
-const shuffleBtn = document.querySelector('#shuffleBtn');
-console.log(shuffleBtn);
 
 
-shuffleBtn.addEventListener('click',shuffle(cards));
+shuffle(cards);
 
 function shuffle(array) {
     var currentIndex = array.length,
@@ -33,17 +31,46 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-    console.log(array);
     return array;
 }
 
 console.log(cards);
 
+// push shuffled array into empty array
+let cardsShuff = [];
+for (let i = 0; i < cardNum; i++) {
+    let cardContent = cards[i];
+    cardsShuff.push(cardContent);
+}
+
+console.log(cardsShuff);
+
+// feed shuffled array into the HTML
+// // // first clear current grid
+const cardDeck = document.querySelector('.deck');
+cardDeck.innerHTML = '';
+console.log(cardDeck);
+// // // insert new shuffled array
+for (let i = 0; i < cardNum; i++) {
+    cardDeck.insertAdjacentHTML('afterbegin', cardsShuff[i].outerHTML);
+}
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ */
+
+ for (let i = 0; i < cardNum; i++) {
+    cardsShuff[i].addEventListener('click',cardReveal(cardsShuff[i])
+    	);
+}
+
+function cardReveal(eachCard) {
+	eachCard.classList.add('open');
+}
+
+ /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
